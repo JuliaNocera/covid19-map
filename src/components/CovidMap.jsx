70825c385fcdb831f78db9c2723e712edb1c35a9
JuliaNocera <nocera.julia@gmail.com>
2021-05-05 09:57:31 -0700

    feat: add numbers to the map

--- src/components/CovidMap.jsx
+++ src/components/CovidMap.jsx
@@ -1,24 +1,37 @@
 import React from "react";
-import { Map, GeoJSON } from "react-leaflet";
+import { Map, GeoJSON, TileLayer, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./CovidMap.css";
 const CovidMap = ({ countries }) => {
   const mapStyle = {
-    fillColor: "white",
+    fillColor: "grey",
     weight: 1,
     color: "black",
-    fillOpacity: 1,
+    fillOpacity: 0.7,
   };
 
   const onEachCountry = (country, layer) => {
     layer.options.fillColor = country.properties.color;
     const name = country.properties.ADMIN;
-    const confirmedText = country.properties.confirmedText;
-    layer.bindPopup(`${name} ${confirmedText}`);
+    console.log()
+    layer.bindTooltip(
+      `
+        <p>${name.length }<p>
+        <p>${name.length - 1} - ${name.length + 2}</p>
+      `,
+      {
+        permanent:true, 
+        direction: "center",
+        className: "my-labels"
+      }).openTooltip();
   };
-
-  return (
-    <Map style={{ height: "90vh" }} zoom={2} center={[20, 60]}>
+return (
+    <Map 
+    style={{ height: "90vh" }} 
+    zoom={2} 
+    center={[20, 60]} 
+    zoomEnd={(e) => console.log('I zoomed', e)}
+    >
       <GeoJSON
         style={mapStyle}
         data={countries}
